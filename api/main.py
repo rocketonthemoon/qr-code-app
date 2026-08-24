181fad67e9c45d8d0bf33e6402e422d3dc827820
salam <pabdulsalam@hotmail.com>
2026-08-24 10:20:06 +0300

    add aws region to force presigned url auth on sigv4

--- api/main.py
+++ api/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
 # AWS S3 Configuration
 s3 = boto3.client(
     's3',
+    region_name= os.getenv("AWS_REGION"),
     aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
     aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
 
